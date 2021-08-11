5d93e969b9dd7ab4624e4dd9fd02b18fc9de1314
AlexeyProjects <62065851+AlexeyProjects@users.noreply.github.com>
2021-08-11 15:40:07 +0400

    1
    1

--- js/main.js
+++ js/main.js
@@ -9,21 +9,21 @@ Date.prototype.addDays = function(days) {
 }
 
 
-let block_today = document.querySelector('.procedure-item.today ')
-let block_twoday = document.querySelector('.procedure-item.twoday ')
-let block_onemonth = document.querySelector('.procedure-item.onemonth ')
-let block_twomonth = document.querySelector('.procedure-item.twomonth ')
-let block_fourmonth = document.querySelector('.procedure-item.fourmonth ')
-let block_sixmonth = document.querySelector('.procedure-item.sixmonth ')
-let block_free = document.querySelector('.procedure-item.freelive ')
+let block_today = document.querySelector('.procedure-item.today')
+let block_twoday = document.querySelector('.procedure-item.twoday')
+let block_onemonth = document.querySelector('.procedure-item.onemonth')
+let block_twomonth = document.querySelector('.procedure-item.twomonth')
+let block_fourmonth = document.querySelector('.procedure-item.fourmonth')
+let block_sixmonth = document.querySelector('.procedure-item.sixmonth')
+let block_free = document.querySelector('.procedure-item.freelive')
 
-let block_today__title = document.querySelector('.procedure-item.today .procedure-item__title')
-let block_twoday__title = document.querySelector('.procedure-item.twoday .procedure-item__title')
-let block_onemonth__title = document.querySelector('.procedure-item.onemonth .procedure-item__title')
-let block_twomonth__title = document.querySelector('.procedure-item.twomonth .procedure-item__title')
-let block_fourmonth__title = document.querySelector('.procedure-item.fourmonth .procedure-item__title')
-let block_sixmonth__title = document.querySelector('.procedure-item.sixmonth .procedure-item__title')
-let block_free__title = document.querySelector('.procedure-item.freelive .procedure-item__title')
+let block_today__title = block_today.querySelector('.procedure-item__title')
+let block_twoday__title = block_twoday.querySelector('.procedure-item__title')
+let block_onemonth__title = block_onemonth.querySelector('.procedure-item__title')
+let block_twomonth__title = block_twomonth.querySelector('.procedure-item__title')
+let block_fourmonth__title = block_fourmonth.querySelector('.procedure-item__title')
+let block_sixmonth__title = block_sixmonth.querySelector('.procedure-item__title')
+let block_free__title = block_free.querySelector('.procedure-item__title')
 
 let btn = document.querySelector('.btn')
 
@@ -90,11 +90,11 @@ block_onemonth__title.textContent = getFullDate(30)
 block_twomonth__title.textContent = getFullDate(60)
 block_fourmonth__title.textContent = getFullDate(120)
 block_sixmonth__title.textContent = getFullDate(180)
+block_sixmonth__title.textContent = getFullDate(180)
 block_free__title.textContent = 'Свободная жизнь'
 
-function addText(block, days) {
-	block.textContent = getFullDate(days)
-}
+
+
 
 
 btn.addEventListener('click', (e) => {
@@ -104,54 +104,45 @@ btn.addEventListener('click', (e) => {
 })
 
 function startAnimate() {
-	function todayLoad() {
-		block_today.classList.add('loaded')
-		setInterval(
-		 ()=>{ block_today.style.color = "rgba(0,0,0,1)" }
-		, 900);
-		
+
+	function textLoad(block) {
+		block.querySelector('.procedure-item__title').classList.add('loaded')
+		block.querySelector('.procedure-item__descr').classList.add('loaded')
 	}
-	function twodayLoad() {
-		block_twoday.classList.add('loaded')
-		setInterval(
-		 ()=>{ block_twoday.style.color = "rgba(0,0,0,1)" }
-		, 900);
-	}
-	function onemonthLoad() {
-		block_onemonth.classList.add('loaded')
-		setInterval(
-		 ()=>{ block_onemonth.style.color = "rgba(0,0,0,1)" }
-		, 900);
-	}
-	function twomonthLoad() {
-		block_twomonth.classList.add('loaded')
-		setInterval(
-		 ()=>{ block_twomonth.style.color = "rgba(0,0,0,1)" }
-		, 900);
-	}
-	function fourmonthLoad() {
-		block_fourmonth.classList.add('loaded')
-		setInterval(
-		 ()=>{ block_fourmonth.style.color = "rgba(0,0,0,1)" }
-		, 900);
-	}
-	function sixmonthLoad() {
-		block_sixmonth.classList.add('loaded')
-		setInterval(
-		 ()=>{ block_sixmonth.style.color = "rgba(0,0,0,1)" }
-		, 900);
-	}
-	function freeliveLoad() {
-		block_free.classList.add('loaded')
-		setInterval(
-		 ()=>{ block_free.style.color = "rgba(0,0,0,1)" ; }
-		, 900);
-	}
-	setTimeout(todayLoad, 300)
-	setTimeout(twodayLoad, 1100)
-	setTimeout(onemonthLoad, 2000)
-	setTimeout(twomonthLoad, 2900)
-	setTimeout(fourmonthLoad, 3800)
-	setTimeout(sixmonthLoad, 4700)
-	setTimeout(freeliveLoad, 5600)
+
+		function todayLoad() {
+			block_today.classList.add('loaded')
+			setTimeout(textLoad, 900, block_today)
+		}
+		function twodayLoad() {
+			block_twoday.classList.add('loaded')
+			setTimeout(textLoad, 900, block_twoday)
+		}
+		function onemonthLoad() {
+			block_onemonth.classList.add('loaded')
+			setTimeout(textLoad, 900, block_onemonth)
+		}
+		function twomonthLoad() {
+			block_twomonth.classList.add('loaded')
+			setTimeout(textLoad, 900, block_twomonth)
+		}
+		function fourmonthLoad() {
+			block_fourmonth.classList.add('loaded')
+			setTimeout(textLoad, 900, block_fourmonth)
+		}
+		function sixmonthLoad() {
+			block_sixmonth.classList.add('loaded')
+			setTimeout(textLoad, 900, block_sixmonth)
+		}
+		function freeblockLoad() {
+			block_free.classList.add('loaded')
+			setTimeout(textLoad, 900, block_free)
+		}
+		setTimeout(todayLoad, 300)
+		setTimeout(twodayLoad, 1200)
+		setTimeout(onemonthLoad, 2100)
+		setTimeout(twomonthLoad, 3000)
+		setTimeout(fourmonthLoad, 3900)
+		setTimeout(sixmonthLoad, 4800)
+		setTimeout(freeblockLoad, 5700)
 }
